d79dd2e0ae2d71a8288e7467061925d188247740
Maks-Katunin <88392581+Maks-Katunin@users.noreply.github.com>
2021-12-08 01:42:22 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/hwa1l1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.geektech.hwa1l1;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.iv_image);
+
+
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(imageView);
+    }
+}
