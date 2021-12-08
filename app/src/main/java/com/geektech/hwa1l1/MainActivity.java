80b2934ee890f629af4c149e40b28e8f5636589d
Maks-Katunin <88392581+Maks-Katunin@users.noreply.github.com>
2021-12-09 01:10:10 +0600

    homework that I completely redone.

--- app/src/main/java/com/geektech/hwa1l1/MainActivity.java
+++ app/src/main/java/com/geektech/hwa1l1/MainActivity.java
@@ -15,8 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.iv_image);
+        // инициализируем изображение
 
 
         Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(imageView);
+        // загружаем изображение из интенета в iv_image
     }
 }
