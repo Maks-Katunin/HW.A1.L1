01769c294dcaf168044b84b6d243cbd7acd1eb71
Maks-Katunin <88392581+Maks-Katunin@users.noreply.github.com>
2021-12-13 02:54:49 +0600

    I need to add the ability to select an image from the phone gallery. need to check for empty username and password length

--- app/src/main/java/com/geektech/hwa1l1/MainActivity.java
+++ app/src/main/java/com/geektech/hwa1l1/MainActivity.java
@@ -2,8 +2,8 @@ package com.geektech.hwa1l1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +12,7 @@ import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText username, password;
     private ImageView imageView;
     private Button btn_Go;
 
@@ -21,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         btn_Go.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Second_Activity.class);
-            startActivity(intent);
+            if (username.getText().toString().length() != 0 && password.getText().toString().length() >= 6){
+                //обрабатываем нажатие на кнопку btn _Go
+                Intent intent = new Intent(MainActivity.this, Second_Activity.class);
+                intent.putExtra("username", username.getText().toString());
+                intent.putExtra("password", password.getText().toString());
+                startActivity(intent);
+            }
+
         });
 
         Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/" +
@@ -31,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        username = findViewById(R.id.et_username);
+        password = findViewById(R.id.et_password);
         btn_Go = findViewById(R.id.button);
         //инициализируем кнопку
         imageView = findViewById(R.id.iv_image);
--- app/src/main/java/com/geektech/hwa1l1/Second_Activity.java
+++ app/src/main/java/com/geektech/hwa1l1/Second_Activity.java
@@ -1,24 +1,70 @@
 package com.geektech.hwa1l1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.Button;
+import android.provider.MediaStore;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 
 public class Second_Activity extends AppCompatActivity {
-
     private ImageView imageView;
+    private TextView editPhoto, username, password;
+    private final int CAMERA_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        imageView = findViewById(R.id.iv_avatar);
+        initViews();
+        intents();
+        editPhoto.setOnClickListener(v -> {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI.getPath());
+                startActivityForResult(intent, CAMERA_REQUEST);
+            } else {
+                requestPermissions(new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA}, 1);
+            }
+        });
+    }
 
-        Glide.with(this).load("https://upload.wikimedia.org/" +
-                "wikipedia/commons/a/a0/Pierre-Person.jpg").into(imageView);
+    private void intents() {
+        String usernameFromMainActivity;
+        String passwordFromMainActivity;
+        usernameFromMainActivity = getIntent().getStringExtra("username");
+        username.setText(usernameFromMainActivity);
+        passwordFromMainActivity = getIntent().getStringExtra("password");
+        password.setText(String.valueOf(passwordFromMainActivity));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK && data != null) {
+
+            Glide.with(this).load(data.getExtras().get("data")).into(imageView);
+
+            /*Bitmap picture = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(picture);*/
+
+        }
+    }
+
+    private void initViews() {
+        username = findViewById(R.id.tv_email);
+        password = findViewById(R.id.tv_password);
+        imageView = findViewById(R.id.iv_avatar);
+        editPhoto = findViewById(R.id.tv_edit_photo);
     }
 }
